f992ff0e6bcd06f02448d213891169bf547afdf8
an thai le <leanthai02@gmail.com>
2025-03-12 08:28:13 +0700

    commit

---
+++ RepositoryUseNHibernate/Mappings/TeacherMap.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+using Shares.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoriesUseNHibernate.Mappings
+{
+    public class TeacherMap : ClassMap<Teacher>
+    {
+        public TeacherMap()
+        {
+            Table("Teacher");
+            Id(x => x.Id);
+            Map(x => x.Name);
+            Map(x => x.DateOfBirth);
+
+            HasMany(x => x.Classes).KeyColumn("TeacherId").Inverse().Cascade.All();
+        }
+    }
+}
